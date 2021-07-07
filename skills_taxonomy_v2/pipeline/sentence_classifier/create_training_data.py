810b9a8148dd7e63e5aa3f1861f9ff7f9ae36c5e
lizgzil <lizgzil@hotmail.com>
2021-07-07 10:01:14 +0100

    Apply black to scripts

--- skills_taxonomy_v2/pipeline/sentence_classifier/create_training_data.py
+++ skills_taxonomy_v2/pipeline/sentence_classifier/create_training_data.py
@@ -8,14 +8,22 @@ from tqdm import tqdm
 import json
 import re
 
-def load_raw_data(jobs_data_file='inputs/karlis_ojo_manually_labelled/OJO_test_labelling_April2021_jobs.jsonl'):
 
-    with open(jobs_data_file, 'r') as file:
+def load_raw_data(
+    jobs_data_file="inputs/karlis_ojo_manually_labelled/OJO_test_labelling_April2021_jobs.jsonl",
+):
+
+    with open(jobs_data_file, "r") as file:
         jobs_data = [json.loads(line) for line in file]
 
     return jobs_data
 
-def mask_text(nlp, text, spacy_ner_types = ['DATE', 'MONEY', 'CARDINAL', 'TIME', 'ORDINAL', 'QUANTITY']):
+
+def mask_text(
+    nlp,
+    text,
+    spacy_ner_types=["DATE", "MONEY", "CARDINAL", "TIME", "ORDINAL", "QUANTITY"],
+):
     # Cleaning where you need to keep indices the same
     doc = nlp(text)
     # '6 months' -> '######'
@@ -24,19 +32,27 @@ def mask_text(nlp, text, spacy_ner_types = ['DATE', 'MONEY', 'CARDINAL', 'TIME',
     # Need to mask with the same length of entity so indexes of skills are still valid
     for entity in reversed(doc.ents):
         if entity.label_ in spacy_ner_types:
-            text = text[:entity.start_char] + '#'*len(str(entity)) + text[entity.end_char:]
-            
+            text = (
+                text[: entity.start_char]
+                + "#" * len(str(entity))
+                + text[entity.end_char :]
+            )
+
     # Replace any * with comma
-    text = text.replace('*', ',')
-    text = text.replace('•', ',')
-    text = text.replace('-', ',')
+    text = text.replace("*", ",")
+    text = text.replace("•", ",")
+    text = text.replace("-", ",")
     return text
+
 
 def text_cleaning(text):
     # Cleaning where it doesnt matter if you mess up the indices
-    text = re.sub(r'[#]+','NUMBER', text)
-    text = re.sub('CNUMBER','C#', text) # Some situations you shouldn't have removed the numbers
+    text = re.sub(r"[#]+", "NUMBER", text)
+    text = re.sub(
+        "CNUMBER", "C#", text
+    )  # Some situations you shouldn't have removed the numbers
     return text
+
 
 def split_labelled_sentences(nlp, text, skills_annotations, skill_label_ids=[1, 5]):
     """
@@ -47,13 +63,17 @@ def split_labelled_sentences(nlp, text, skills_annotations, skill_label_ids=[1, 
     - Create list of sentences with corresponding list of tags
     """
 
-    skill_spans = [(label['start_offset'], label['end_offset']) for label in skills_annotations if label['label'] in skill_label_ids]
-    
+    skill_spans = [
+        (label["start_offset"], label["end_offset"])
+        for label in skills_annotations
+        if label["label"] in skill_label_ids
+    ]
+
     # Mask out numbers (good to do before sentence splitting since they can look like sentence boundaries) and remove *, but doesn't effect number of characters
     text = mask_text(nlp, text)
-    
+
     # Split up sentences
-    skill_span_sets = [set(range(s,e)) for s,e in skill_spans]
+    skill_span_sets = [set(range(s, e)) for s, e in skill_spans]
     doc = nlp(text)
     sentences = []
     sentences_label = []
@@ -67,7 +87,10 @@ def split_labelled_sentences(nlp, text, skills_annotations, skill_label_ids=[1, 
 
     return sentences, sentences_label
 
-def create_training_data(nlp, jobs_data, sentence_train_threshold=15, skill_label_ids=[1, 5]):
+
+def create_training_data(
+    nlp, jobs_data, sentence_train_threshold=15, skill_label_ids=[1, 5]
+):
     """
     Label all job adverts and add them all to a list of training data.
     sentence_train_threshold: A threshold of how big the sentence has to be in order to include it in the training/test data
@@ -76,32 +99,41 @@ def create_training_data(nlp, jobs_data, sentence_train_threshold=15, skill_labe
     # Create training dataset
     training_data = []
     for job_info in tqdm(jobs_data):
-        text = job_info['text']
-        skills_annotations = job_info['annotations']
-        sentences, sentences_label = split_labelled_sentences(nlp, text, skills_annotations, skill_label_ids=skill_label_ids)
+        text = job_info["text"]
+        skills_annotations = job_info["annotations"]
+        sentences, sentences_label = split_labelled_sentences(
+            nlp, text, skills_annotations, skill_label_ids=skill_label_ids
+        )
         for sentence, sentence_label in zip(sentences, sentences_label):
             if len(sentence) > sentence_train_threshold:
                 training_data.append((sentence, sentence_label))
 
     return training_data
 
+
 def save_training_data(training_data, output_file):
-    with open(output_file, 'w') as file:
+    with open(output_file, "w") as file:
         for label in training_data:
             file.write(json.dumps(label))
-            file.write('\n')
+            file.write("\n")
+
 
 def load_training_data(input_file):
-    with open(input_file, 'r') as file:
+    with open(input_file, "r") as file:
         training_data = [tuple(json.loads(line)) for line in file]
     return training_data
 
-if __name__ == '__main__':
-    
-    jobs_data = load_raw_data(jobs_data_file='inputs/karlis_ojo_manually_labelled/OJO_test_labelling_April2021_jobs.jsonl')
+
+if __name__ == "__main__":
+
+    jobs_data = load_raw_data(
+        jobs_data_file="inputs/karlis_ojo_manually_labelled/OJO_test_labelling_April2021_jobs.jsonl"
+    )
 
     nlp = spacy.load("en_core_web_sm")
 
     training_data = create_training_data(nlp, jobs_data, sentence_train_threshold=15)
 
-    save_training_data(training_data, 'outputs/sentence_classifier/data/training_data_April2021.json')
+    save_training_data(
+        training_data, "outputs/sentence_classifier/data/training_data_April2021.json"
+    )
--- skills_taxonomy_v2/pipeline/tests/test_create_training_data.py
+++ skills_taxonomy_v2/pipeline/tests/test_create_training_data.py
@@ -1,55 +1,79 @@
-
 import spacy
 
 import pytest
 
 from skills_taxonomy_v2.pipeline.sentence_classifier.create_training_data import (
-	mask_text,
-	text_cleaning,
-	split_labelled_sentences,
-	create_training_data
-	)
+    mask_text,
+    text_cleaning,
+    split_labelled_sentences,
+    create_training_data,
+)
 
 nlp = spacy.load("en_core_web_sm")
 
+
 def test_mask_text():
 
-	text = 'The £1000 which is 23 times the money made at 8am, 8:00am, and 08:00. And now • bullet 1 • bullet 2'
-	expected_text = 'The £#### which is ## times the money made at ###, ######, and #####. And now , bullet 1 , bullet 2'
+    text = "The £1000 which is 23 times the money made at 8am, 8:00am, and 08:00. And now • bullet 1 • bullet 2"
+    expected_text = "The £#### which is ## times the money made at ###, ######, and #####. And now , bullet 1 , bullet 2"
 
-	masked_text = mask_text(nlp, text, spacy_ner_types = ['DATE', 'MONEY', 'CARDINAL', 'TIME', 'ORDINAL', 'QUANTITY'])
+    masked_text = mask_text(
+        nlp,
+        text,
+        spacy_ner_types=["DATE", "MONEY", "CARDINAL", "TIME", "ORDINAL", "QUANTITY"],
+    )
 
-	assert len(masked_text) == len(text)
-	assert masked_text == expected_text
+    assert len(masked_text) == len(text)
+    assert masked_text == expected_text
+
 
 def test_text_cleaning():
 
-	text = 'This is #### a ## and ######'
-	cleaned_text = text_cleaning(text)
-	expected_text = 'This is NUMBER a NUMBER and NUMBER'
+    text = "This is #### a ## and ######"
+    cleaned_text = text_cleaning(text)
+    expected_text = "This is NUMBER a NUMBER and NUMBER"
 
-	assert expected_text == expected_text
+    assert expected_text == expected_text
+
 
 def test_split_labelled_sentences():
 
-	text = 'this is a skill sentence. and another skill here too. here is a dog though.'
-	skills_annotations = [
-		{'start_offset': 10, 'end_offset': 15, 'label': 1},
-		{'start_offset': 38, 'end_offset': 43, 'label': 1},
-		{'start_offset': 64, 'end_offset': 67, 'label': 2}]
-	sentences, sentences_label = split_labelled_sentences(nlp, text, skills_annotations, skill_label_ids=[1])
+    text = "this is a skill sentence. and another skill here too. here is a dog though."
+    skills_annotations = [
+        {"start_offset": 10, "end_offset": 15, "label": 1},
+        {"start_offset": 38, "end_offset": 43, "label": 1},
+        {"start_offset": 64, "end_offset": 67, "label": 2},
+    ]
+    sentences, sentences_label = split_labelled_sentences(
+        nlp, text, skills_annotations, skill_label_ids=[1]
+    )
 
-	assert len(sentences) == len(sentences_label)
-	assert len(sentences) == 3
-	assert sentences_label == [1, 1, 0]
+    assert len(sentences) == len(sentences_label)
+    assert len(sentences) == 3
+    assert sentences_label == [1, 1, 0]
+
 
 def test_create_training_data():
 
-	jobs_data = [
-		{'text': 'hi. this is a skill.', 'annotations': [{'start_offset': 14, 'end_offset': 19, 'label': 1}]},
-		{'text': 'and another skill. hello.', 'annotations': [{'start_offset': 12, 'end_offset': 17, 'label': 1}]}
-		]
-	training_data = create_training_data(nlp, jobs_data, sentence_train_threshold=3, skill_label_ids=[1])
-	expected_training_data = [('this is a skill.', 1), ('and another skill.', 1), ('hello.', 0)]
+    jobs_data = [
+        {
+            "text": "hi. this is a skill.",
+            "annotations": [{"start_offset": 14, "end_offset": 19, "label": 1}],
+        },
+        {
+            "text": "and another skill. hello.",
+            "annotations": [{"start_offset": 12, "end_offset": 17, "label": 1}],
+        },
+    ]
+    training_data = create_training_data(
+        nlp, jobs_data, sentence_train_threshold=3, skill_label_ids=[1]
+    )
+    expected_training_data = [
+        ("this is a skill.", 1),
+        ("and another skill.", 1),
+        ("hello.", 0),
+    ]
 
-	assert sorted(expected_training_data, key=lambda x: x[0]) == sorted(training_data, key=lambda x: x[0])
+    assert sorted(expected_training_data, key=lambda x: x[0]) == sorted(
+        training_data, key=lambda x: x[0]
+    )
