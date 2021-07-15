e81d86226370220d7f1a0fa1aab8b5bd6e81cd39
lizgzil <lizgzil@hotmail.com>
2021-07-15 13:50:27 +0100

    Get word embeddings using spacy

--- skills_taxonomy_v2/pipeline/skills_extraction/get_word_embeddings.py
+++ skills_taxonomy_v2/pipeline/skills_extraction/get_word_embeddings.py
@@ -3,16 +3,15 @@ Input sentences output word embeddings for each of them
 """
 
 import pickle
+import re
+import json
+import os
 
-from transformers import BertTokenizer, BertModel
+# from transformers import BertTokenizer, BertModel
 
-from skills_taxonomy_v2.pipeline.skills_extraction.cleaning_sentences import (
-    deduplicate_sentences,
-    sentences2cleantokens,
-    build_ngrams,
-    get_common_tuples,
-    remove_common_tuples
-)
+from skills_taxonomy_v2.pipeline.skills_extraction.cleaning_sentences import separate_camel_case, deduplicate_sentences
+
+import cupy
 import spacy
 import torch
 import numpy
@@ -33,23 +32,21 @@ if __name__ == '__main__':
 	config_name = '2021.07.09.small'
 
 	with open(
-	    f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_predictions.pkl", 
-	    "rb") as file:
-	    sentences_pred = pickle.load(file)
-	    
+		f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_predictions.pkl", 
+		"rb") as file:
+		sentences_pred = pickle.load(file)
+		
 	with open(
-	    f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_sentences.pkl",
-	    "rb") as file:
-	    sentences = pickle.load(file)
+		f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_sentences.pkl",
+		"rb") as file:
+		sentences = pickle.load(file)
 
 	with open(
-	    f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_job_ids.pkl",
-	    "rb") as file:
-	    job_ids = pickle.load(file)
+		f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_job_ids.pkl",
+		"rb") as file:
+		job_ids = pickle.load(file)
 
 	token_len_threshold = 20 # To adjust. I had a look and > this number seems to all be not words (urls etc)
-	lemma_n = 2
-	top_n_common_remove = 20
 
 	# Filter out the non-skill sentences
 	sentences = [sentences[i] for i,p in enumerate(sentences_pred.astype(bool)) if p==1]
@@ -60,42 +57,44 @@ if __name__ == '__main__':
 	# Get embeddings for each token in the sentence
 	nlp = spacy.load("en_core_web_trf")
 
-	for doc in nlp.pipe(["some text", "some other text"]):
-    	tokvecs = doc._.trf_data.tensors[-1]
+	output_directory = 'outputs/skills_extraction/data/'
+	if not os.path.exists(output_directory):
+		os.makedirs(output_directory)
 
-	# lemma_sentences = []
-	# clean_sentences = []
-	# tokvecs_sentences = []
-	# for sentence in sentences:
-	#     sentence = separate_camel_case(sentence)
-	#     doc = nlp(sentence)
-	#     tokvecs = doc._.trf_data.tensors[-1]
-	#     tokens = doc._.trf_word_pieces_  # String values of the wordpieces
-	#     # Only append tokens/embeddings for words which are ok
-	#     # Don't include very long words
-	#     # or proper nouns/numbers
-	#     # or words with numbers in (these are always garbage)
-	#     # You generally take out a lot of the urls by having a token_len_threshold but not always
-	#     lemma_sentence = []
-	#     clean_sentence = []
-	#     tokvecs_sentence = []
-	#     for token in doc:
-	#         # Don't include very long words
-	#         # or proper nouns/numbers
-	#         # or words with numbers in (these are always garbage)
-	#         # You generally take out a lot of the urls by having a token_len_threshold but not always
-	#         if (
-	#             ("www" not in token.text)
-	#             and (len(token) < token_len_threshold)
-	#             and (token.pos_ not in ["PROPN", "NUM", "SPACE"])
-	#             and (not re.search("\d", token.text))
-	#             and (not token.text in stopwords.words())
-	#         ):
-	#             lemma_sentence.append(token.lemma_.lower())
-	#             clean_sentence.append(token.text.lower())
-	#     if lemma_sentence:
-	#         lemma_sentences.append(lemma_sentence)
-	#     if clean_sentence:
-	#         clean_sentences.append(clean_sentence)
+	for job_id, sentence in zip(job_ids, sentences):
+		sentence = separate_camel_case(sentence)
+		doc = nlp(sentence)
+		tokvecs = doc._.trf_data.tensors[0][0]
+		lemma_sentence = []
+		clean_sentence = []
+		tokvecs_i = []
+		for i, token in enumerate(doc):
+			# Don't include very long words
+			# or proper nouns/numbers
+			# or words with numbers in (these are always garbage)
+			# You generally take out a lot of the urls by having a token_len_threshold but not always
+			if (
+				("www" not in token.text)
+				and (len(token) < token_len_threshold)
+				and (token.pos_ not in ["PROPN", "NUM", "SPACE"])
+				and (not re.search("\d", token.text))
+				and (not token.text in stopwords.words())
+			 ):
+				lemma_sentence.append(token.lemma_.lower())
+				clean_sentence.append(token.text.lower())
+				tokvecs_i.append(i)
+		if clean_sentence:
+			output_line = {
+				'job_id': job_id,
+				'cleaned sentence': clean_sentence,
+				'lemmatized sentence': lemma_sentence,
+				'word embeddings': tokvecs[tokvecs_i].tolist()
+				}
+			with open(os.path.join(output_directory, '{config_name}_jobsnew1_transformers_word_embeddings.jsonl'), "a") as file:
+				file.write(json.dumps(output_line))
+				file.write("\n")
+
+		
+
 
 
