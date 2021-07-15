357010eb744c80acb367be32523cec6333c278e6
lizgzil <lizgzil@hotmail.com>
2021-07-15 14:14:55 +0100

    Apply black to skill embeddings

--- skills_taxonomy_v2/pipeline/skills_extraction/get_word_embeddings.py
+++ skills_taxonomy_v2/pipeline/skills_extraction/get_word_embeddings.py
@@ -8,9 +8,13 @@ import json
 import os
 
 from tqdm import tqdm
+
 # from transformers import BertTokenizer, BertModel
 
-from skills_taxonomy_v2.pipeline.skills_extraction.cleaning_sentences import separate_camel_case, deduplicate_sentences
+from skills_taxonomy_v2.pipeline.skills_extraction.cleaning_sentences import (
+    separate_camel_case,
+    deduplicate_sentences,
+)
 
 import cupy
 import spacy
@@ -22,75 +26,86 @@ from nltk.corpus import stopwords
 
 nltk.download("stopwords")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-	# Use the GPU, with memory allocations directed via PyTorch.
-	# This prevents out-of-memory errors that would otherwise occur from competing
-	# memory pools.
-	set_gpu_allocator("pytorch")
-	require_gpu(0)
-	
-	config_name = '2021.07.09.small'
+    # Use the GPU, with memory allocations directed via PyTorch.
+    # This prevents out-of-memory errors that would otherwise occur from competing
+    # memory pools.
+    set_gpu_allocator("pytorch")
+    require_gpu(0)
 
-	with open(
-		f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_predictions.pkl", 
-		"rb") as file:
-		sentences_pred = pickle.load(file)
-		
-	with open(
-		f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_sentences.pkl",
-		"rb") as file:
-		sentences = pickle.load(file)
+    config_name = "2021.07.09.small"
 
-	with open(
-		f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_job_ids.pkl",
-		"rb") as file:
-		job_ids = pickle.load(file)
+    with open(
+        f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_predictions.pkl",
+        "rb",
+    ) as file:
+        sentences_pred = pickle.load(file)
 
-	token_len_threshold = 20 # To adjust. I had a look and > this number seems to all be not words (urls etc)
+    with open(
+        f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_sentences.pkl",
+        "rb",
+    ) as file:
+        sentences = pickle.load(file)
 
-	# Filter out the non-skill sentences
-	sentences = [sentences[i] for i,p in enumerate(sentences_pred.astype(bool)) if p==1]
+    with open(
+        f"outputs/sentence_classifier/data/skill_sentences/{config_name}_jobsnew1_job_ids.pkl",
+        "rb",
+    ) as file:
+        job_ids = pickle.load(file)
 
-	# Deduplicate sentences
-	sentences = deduplicate_sentences(sentences)
+    token_len_threshold = 20  # To adjust. I had a look and > this number seems to all be not words (urls etc)
 
-	# Get embeddings for each token in the sentence
-	nlp = spacy.load("en_core_web_trf")
+    # Filter out the non-skill sentences
+    sentences = [
+        sentences[i] for i, p in enumerate(sentences_pred.astype(bool)) if p == 1
+    ]
 
-	output_directory = 'outputs/skills_extraction/data/'
-	if not os.path.exists(output_directory):
-		os.makedirs(output_directory)
+    # Deduplicate sentences
+    sentences = deduplicate_sentences(sentences)
 
-	for job_id, sentence in tqdm(zip(job_ids, sentences)):
-		sentence = separate_camel_case(sentence)
-		doc = nlp(sentence)
-		tokvecs = doc._.trf_data.tensors[0][0]
-		lemma_sentence = []
-		clean_sentence = []
-		tokvecs_i = []
-		for i, token in enumerate(doc):
-			# Don't include very long words
-			# or proper nouns/numbers
-			# or words with numbers in (these are always garbage)
-			# You generally take out a lot of the urls by having a token_len_threshold but not always
-			if (
-				("www" not in token.text)
-				and (len(token) < token_len_threshold)
-				and (token.pos_ not in ["PROPN", "NUM", "SPACE"])
-				and (not re.search("\d", token.text))
-				and (not token.text in stopwords.words())
-			 ):
-				lemma_sentence.append(token.lemma_.lower())
-				clean_sentence.append(token.text.lower())
-				tokvecs_i.append(i)
-		if clean_sentence:
-			output_line = {
-				'job_id': job_id,
-				'cleaned sentence': clean_sentence,
-				'lemmatized sentence': lemma_sentence,
-				'word embeddings': tokvecs[tokvecs_i].tolist()
-				}
-			with open(os.path.join(output_directory, f'{config_name}_jobsnew1_transformers_word_embeddings.jsonl'), "a") as file:
-				file.write(json.dumps(output_line))
-				file.write("\n")
+    # Get embeddings for each token in the sentence
+    nlp = spacy.load("en_core_web_trf")
+
+    output_directory = "outputs/skills_extraction/data/"
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    for job_id, sentence in tqdm(zip(job_ids, sentences)):
+        sentence = separate_camel_case(sentence)
+        doc = nlp(sentence)
+        tokvecs = doc._.trf_data.tensors[0][0]
+        lemma_sentence = []
+        clean_sentence = []
+        tokvecs_i = []
+        for i, token in enumerate(doc):
+            # Don't include very long words
+            # or proper nouns/numbers
+            # or words with numbers in (these are always garbage)
+            # You generally take out a lot of the urls by having a token_len_threshold but not always
+            if (
+                ("www" not in token.text)
+                and (len(token) < token_len_threshold)
+                and (token.pos_ not in ["PROPN", "NUM", "SPACE"])
+                and (not re.search("\d", token.text))
+                and (not token.text in stopwords.words())
+            ):
+                lemma_sentence.append(token.lemma_.lower())
+                clean_sentence.append(token.text.lower())
+                tokvecs_i.append(i)
+        if clean_sentence:
+            output_line = {
+                "job_id": job_id,
+                "cleaned sentence": clean_sentence,
+                "lemmatized sentence": lemma_sentence,
+                "word embeddings": tokvecs[tokvecs_i].tolist(),
+            }
+            with open(
+                os.path.join(
+                    output_directory,
+                    f"{config_name}_jobsnew1_transformers_word_embeddings.jsonl",
+                ),
+                "a",
+            ) as file:
+                file.write(json.dumps(output_line))
+                file.write("\n")
