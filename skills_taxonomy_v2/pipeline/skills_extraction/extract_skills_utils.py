566695ff960835fde5a1a0f07c4c06809541f7d3
lizgzil <lizgzil@hotmail.com>
2021-09-06 13:28:23 +0100

    Remove naming skills functions from extract skills utils and only fit reducer class on 100k sentences

--- skills_taxonomy_v2/pipeline/skills_extraction/extract_skills_utils.py
+++ skills_taxonomy_v2/pipeline/skills_extraction/extract_skills_utils.py
@@ -1,10 +1,9 @@
 """
-Functions to reduce embeddings, apply DSCAN clustering and name clusters using
-top TF-IDF words. Used in extract_skills.py.
+Functions to reduce embeddings, and apply DSCAN clustering. Used in extract_skills.py.
 """
 
 import logging
-from collections import Counter, defaultdict
+from collections import defaultdict
 import os
 import joblib
 import tempfile
@@ -12,36 +11,15 @@ import random
 
 import numpy as np
 from tqdm import tqdm
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.cluster import DBSCAN
 import umap.umap_ as umap
 import re
-import nltk
-from nltk.util import ngrams  # function for making ngrams
-from nltk.stem import WordNetLemmatizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-from sklearn.metrics.pairwise import euclidean_distances
 
 from skills_taxonomy_v2.getters.s3_data import load_s3_data, save_to_s3
 from skills_taxonomy_v2 import BUCKET_NAME
 
 logger = logging.getLogger(__name__)
-
-nltk.download('wordnet')
-
-def replace_ngrams(sentence, ngram_words):
-    for word_list in ngram_words:
-        sentence = sentence.replace(" ".join(word_list), "-".join(word_list))
-    return sentence
-
-
-def get_top_tf_idf_words(clusters_vect, feature_names, top_n=2):
-    """
-    From https://stackoverflow.com/questions/34232190/scikit-learn-tfidfvectorizer-how-to-get-top-n-terms-with-highest-tf-idf-score
-    """
-    sorted_nzs = np.argsort(clusters_vect.data)[: -(top_n + 1) : -1]
-    return feature_names[clusters_vect.indices[sorted_nzs]]
 
 
 def sample_sentence_embeddings_dirs(sentence_embeddings_dirs, sample_size, sample_seed=42):
@@ -124,153 +102,6 @@ def load_sentences_embeddings(
 
     return sentences_data
 
-
-def clean_cluster_descriptions(sentences_data):
-    """
-    For each cluster normalise the texts for getting descriptions from
-    - lemmatize
-    - lower case
-    - remove duplicates
-    - n-grams
-
-    Input:
-        sentences_data (DataFrame): The sentences in each cluster
-            with "description" and "Cluster number" columns
-
-    Output:
-        cluster_descriptions (dict): Cluster number : list of cleaned
-            sentences in this cluster
-    """
-
-    # Init the Wordnet Lemmatizer
-    lemmatizer = WordNetLemmatizer()
-
-    # How many times a n-gram has to occur in order for occurences of them
-    # to be converted to a single dash separated word
-    num_times_ngrams_thresh = 3
-
-    sentences_data["description"] = sentences_data["description"].apply(
-        lambda x: re.sub("\s+", " ", x)
-    )
-
-    cluster_descriptions = {}
-    for cluster_num, cluster_group in sentences_data.groupby("Cluster number"):
-        cluster_docs = cluster_group["description"].tolist()
-        cluster_docs_cleaned = []
-        for doc in cluster_docs:
-            # Remove capitals, but not when it's an acronym
-            acronyms = re.findall("[A-Z]{2,}", doc)
-            # Lemmatize
-            lemmatized_output = [
-                lemmatizer.lemmatize(w)
-                if w in acronyms
-                else lemmatizer.lemmatize(w.lower())
-                for w in doc.split(" ")
-            ]
-            cluster_docs_cleaned.append(" ".join(lemmatized_output).strip())
-        # Remove duplicates
-        cluster_docs_cleaned = list(set(cluster_docs_cleaned))
-
-        # Find the ngrams for this cluster
-        all_cluster_docs = " ".join(cluster_docs_cleaned).split(" ")
-
-        esBigrams = ngrams(all_cluster_docs, 3)
-        ngram_words = [
-            words
-            for words, count in Counter(esBigrams).most_common()
-            if count >= num_times_ngrams_thresh
-        ]
-
-        cluster_docs_clean = [
-            replace_ngrams(sentence, ngram_words) for sentence in cluster_docs_cleaned
-        ]
-
-        cluster_descriptions[cluster_num] = cluster_docs_clean
-    return cluster_descriptions
-
-
-def get_skill_info(sentences_data, desc_num_top_sent, name_num_top_words):
-    """
-    For each cluster/skill get a description and a name for it by:
-    - Cleaning words (lemmatize...)
-    - Name is top 5 tfidf words in the sentences for this cluster
-    - Description is the closest original sentence with the embedding
-    closest to the cluster centre
-
-    Input:
-        sentences_data (DataFrame): The sentences in each cluster
-            with "description" and "Cluster number" columns
-        desc_num_top_sent (int): How many original sentences closest to
-            the cluster centroid to include as the skill description.
-        name_num_top_words (int): How many top TF-IDF words to merge
-            together as the name for clusters
-    Output:
-        skills_data (dict): Information about each skill -
-            "Skill name": The top TF-IDF words merged together for each skill,
-            "Description": The skill description - closest sentences to the
-                cluster centre,
-            "text": The text for each sentence that created this skill cluster
-
-
-    """
-
-    # Clean sentences
-    cluster_descriptions = clean_cluster_descriptions(sentences_data)
-
-    # Find the closest-to-the-cluster-center sentences in this cluster
-
-    cluster_texts = [
-        ". ".join(sentences) for sentences in cluster_descriptions.values()
-    ]
-
-    # Have a token pattern that keeps in words with dashes in between
-    cluster_vectorizer = TfidfVectorizer(
-        stop_words="english", token_pattern=r"(?u)\b\w[\w-]*\w\b|\b\w+\b"
-    )
-    clusters_vects = cluster_vectorizer.fit_transform(cluster_texts)
-
-    cluster_main_sentence = {}
-    for cluster_num, cluster_data in sentences_data.groupby("Cluster number"):
-        # There may be the same sentence repeated
-        cluster_data.drop_duplicates(["sentence id"], inplace=True)
-        cluster_text = cluster_data["original sentence"].tolist()
-        cluster_coords = cluster_data[["reduced_points x", "reduced_points y"]].values
-
-        # Get similarities to centre
-        similarities = cosine_similarity(
-            np.mean(cluster_coords, axis=0).reshape(1, -1), cluster_coords
-        )
-        # Join the closest sentences to the centroid
-        cluster_main_sentence[cluster_num] = ". ".join(
-            [
-                cluster_text[i]
-                for i in similarities.argsort()[0][::-1].tolist()[0:desc_num_top_sent]
-            ]
-        )
-
-    # Top n words for each cluster + other info
-    feature_names = np.array(cluster_vectorizer.get_feature_names())
-
-    # Get top words for each cluster
-    skills_data = {}
-    for (cluster_num, text), clusters_vect in zip(
-        cluster_descriptions.items(), clusters_vects
-    ):
-        skills_data[cluster_num] = {
-            "Skill name": " ".join(
-                list(
-                    get_top_tf_idf_words(
-                        clusters_vect, feature_names, name_num_top_words
-                    )
-                )
-            ),
-            "Description": cluster_main_sentence[cluster_num],
-            "text": text,
-        }
-
-    return skills_data
-
-
 def get_output_config_stamped(config_path, output_dir, filename_suffix):
     """
     You may want to have the name of the config file that was used to generate
@@ -319,7 +150,16 @@ class ExtractSkills(object):
             random_state=self.umap_random_state,
             n_components=self.umap_n_components,
         )
-        reduced_points_umap = self.reducer_class.fit_transform(embedding_list)
+        if len(embedding_list) >= 100000:
+            # If there is a lot of data, then fit the reducer class to a sample of the data 
+            # since it takes up too much memory for saving otherwise.
+            # Then transform all of the points.
+            random.seed(42)
+            embedding_list_sample = random.sample(embedding_list, 100000)
+            self.reducer_class.fit(embedding_list_sample)
+            reduced_points_umap = self.reducer_class.transform(embedding_list)
+        else:
+            reduced_points_umap = self.reducer_class.fit_transform(embedding_list)
 
         return reduced_points_umap
 
@@ -357,7 +197,7 @@ class ExtractSkills(object):
 
         logger.info(f"{len(set(clustering_number))} unique clusters")
 
-        return clustering_number, self.cluster_centroids
+        return clustering_number
 
     def save_outputs(
         self,
