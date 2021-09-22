eb70c1b8fffa6ec3389ba9648bd08575ccc277b0
lizgzil <lizgzil@hotmail.com>
2021-09-22 12:12:21 +0100

    efficiency increase to filter_bulk_data.py

--- skills_taxonomy_v2/pipeline/tk_data_analysis/filter_bulk_data.py
+++ skills_taxonomy_v2/pipeline/tk_data_analysis/filter_bulk_data.py
@@ -4,12 +4,13 @@ Quick script to filter all bulk data to just save job ids which we used in our s
 
 from tqdm import tqdm
 import json
+import os
 
 import numpy as np
 import pandas as pd
 import boto3
 
-from skills_taxonomy_v2.getters.s3_data import load_s3_data
+from skills_taxonomy_v2.getters.s3_data import load_s3_data, save_to_s3
 from skills_taxonomy_v2 import BUCKET_NAME
 
 s3 = boto3.resource("s3")
@@ -23,25 +24,23 @@ if __name__ == '__main__':
 	sentence_data = sentence_data[sentence_data['Cluster number']!=-1]
 
 	# Job adverts in our sample
-	skill_job_ads = list(sentence_data['job id'].unique())
+	skill_job_ads = set(sentence_data['job id'].unique())
 
 	# Job titles
 	job_titles = {}
 	for file_name in tqdm(range(0,13)):
-	    file_date_dict = load_s3_data(
+	    file_dict = load_s3_data(
 	        s3,
 	        BUCKET_NAME,
 	        f'outputs/tk_data_analysis/metadata_job/{file_name}.json')
-	    for job_id, f in file_date_dict.items():
-	        if job_id in skill_job_ads:
-	            job_titles[job_id] = f
-	    
+	    job_titles.update({job_id: f for job_id, f in file_dict.items() if job_id in skill_job_ads})
+    
 	print(len(job_titles))
 	save_to_s3(
 		s3,
 		BUCKET_NAME,
 		job_titles,
-		os.path.join('outputs/tk_data_analysis/metadata_job/sample_filtered.json')
+		'outputs/tk_data_analysis/metadata_job/sample_filtered.json'
 		)
 
 	# Job dates
@@ -51,14 +50,12 @@ if __name__ == '__main__':
 	        s3,
 	        BUCKET_NAME,
 	        f'outputs/tk_data_analysis/metadata_date/{file_name}.json')
-	    for job_id, f in file_date_dict.items():
-	        if job_id in skill_job_ads:
-	            job_dates[job_id] = f[0]
+	    job_dates.update({job_id: f[0] for job_id, f in file_date_dict.items() if job_id in skill_job_ads})
 	    
 	print(len(job_dates))
 	save_to_s3(
 		s3,
 		BUCKET_NAME,
 		job_dates,
-		os.path.join('outputs/tk_data_analysis/metadata_date/sample_filtered.json')
+		'outputs/tk_data_analysis/metadata_date/sample_filtered.json'
 		)
