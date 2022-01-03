95f01c5feada528e627ddaab51ea24d3e3567a7c
lizgzil <lizgzil@hotmail.com>
2022-01-03 15:57:03 +0000

    Get no text and full text counts

--- skills_taxonomy_v2/pipeline/tk_data_analysis/get_no_texts_tk_data.py
+++ skills_taxonomy_v2/pipeline/tk_data_analysis/get_no_texts_tk_data.py
@@ -16,6 +16,9 @@ from skills_taxonomy_v2.getters.s3_data import get_s3_data_paths, load_s3_data, 
 from skills_taxonomy_v2 import BUCKET_NAME
 
 s3 = boto3.resource("s3")
+ 
+# 1. Get counts when there is the full text field
+# 2. Get job ids when there is no full text field
 
 tk_data_path = "inputs/data/textkernel-files/"
 
@@ -23,13 +26,37 @@ all_tk_data_paths = get_s3_data_paths(
     s3, BUCKET_NAME, tk_data_path, file_types=["*.jsonl*"]
 )
 
+job_dates_fulltext_count = defaultdict(int)
+job_region_fulltext_count = defaultdict(int)
 no_text_job_ids = []
 for file_name in tqdm(all_tk_data_paths):
     data = load_s3_data(s3, BUCKET_NAME, file_name)
     for d in data:
-    	if not d.get('full_text'):
-    		no_text_job_ids.append(d.get('job_id'))
+        if d.get('full_text'):
+            if d.get("date"):
+                job_dates_fulltext_count[d.get("date")] += 1
+            else:
+                job_dates_fulltext_count["Not given"] += 1
+            region = d.get("region")
+            if region:
+                region_label = region.get("label")
+                if region_label:
+                    job_region_fulltext_count[region_label] += 1
+        else:
+            no_text_job_ids.append(d.get('job_id'))
 
+save_to_s3(
+        s3,
+        BUCKET_NAME,
+        job_dates_fulltext_count,
+        f"outputs/tk_data_analysis_new_method/metadata_date/tk_dates_count_got_full_text.json",
+    )
+save_to_s3(
+        s3,
+        BUCKET_NAME,
+        job_region_fulltext_count,
+        f"outputs/tk_data_analysis_new_method/metadata_location/tk_regions_count_got_full_text.json",
+    )
 save_to_s3(
         s3,
         BUCKET_NAME,
